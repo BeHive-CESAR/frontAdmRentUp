2494e17b2b017c8ee247cf492a879b98a6c10cb8
Batatta <carlos.mec2005@gmail.com>
2023-11-01 23:53:20 -0300

    Finalização da tela de inventário

--- Inventário.py
+++ Inventário.py
@@ -3,90 +3,146 @@ import requests
 from PIL import Image
 import pandas as pd
 import numpy as np
+import json
 
-#streamlit run myfile.py
+def inventario():
+    #streamlit run myfile.py
 
-with open('style.css') as file:
-    st.markdown(f'<style>{file.read()}</style>', unsafe_allow_html=True)
+    with open('style.css') as file:
+        st.markdown(f'<style>{file.read()}</style>', unsafe_allow_html=True)
 
-st.markdown(
-    """
-    <style>
-        section[data-testid="stSidebar"] {
-            width: 200px !important; # Set the width to your desired value
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+    st.markdown(
+        """
+        <style>
+            section[data-testid="stSidebar"] {
+                width: 200px !important; # Set the width to your desired value
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
-# SIDE BAR #
-with st.sidebar:
-    st.markdown("##")
-    st.text("Olá, Fulano!")
+    # SIDE BAR #
+    with st.sidebar:
+        st.markdown("##")
+        st.text("Olá, Fulano!")
 
 
-# HEADER DO SITE #
-logo_container = st.container()
-col1, col2, col3 = st.columns(3)
+    # HEADER DO SITE #
+    logo_container = st.container()
+    col1, col2, col3 = st.columns(3)
 
-with logo_container:
-    with col2:
-        image = Image.open('img/logo.png')
-        st.image(image, width=150)
- 
-st.subheader('Inventário', divider='orange')
-
-searchBar_container = st.container()
-with searchBar_container:
-
-    # BARRA DE PESQUISA #
-    searchInput = st.text_input("",placeholder="Buscar Item")
+    with logo_container:
+        with col2:
+            image = Image.open('img/logo.png')
+            st.image(image, width=150)
     
-    # FORMS DE ADICIONAR ITEM #
-    with st.expander("Adicionar Item"):
-        with st.form("Adicionar", True):
-            url = 'https://mockapi.up.railway.app/post-item'
-            nome = st.text_input('Nome')
-            total = st.number_input('Total', value = None, step=1)
-            estoque = st.number_input('Estoque', value = None, step=1)
-            emprestimo = st.number_input('Emprestáveis', value = None,step=1)
-            emprestados = st.number_input('Emprestados', value = None, step=1)
-            quebrados = st.number_input('Quebrados',value = None, step=1)
-            descricao = st.text_input('Descrição')
+    st.subheader('Inventário', divider='orange')
 
-        # Every form must have a submit button.
-            submitted = st.form_submit_button("Enviar")
-            if submitted:
-                    data = {
-                        "item" : nome,
-                        "qnt_total": total,
-                        "qnt_estoque": estoque,
-                        "qnt_emprestimo": emprestimo,
-                        "qnt_emprestados": emprestados,
-                        "qnt_quebrados": quebrados,
-                        "descricao": descricao
-                    }
-                    response = requests.post(url, json=data)
+    searchBar_container = st.container()
+    with searchBar_container:
 
+        # BARRA DE PESQUISA #
+        url = 'https://mockapi.up.railway.app/get_itens'
+        requestData = requests.get(url).json()
+        df = pd.DataFrame.from_dict(requestData)
+        array_nomes = df.values[:, 0]
+        searchInput= st.selectbox('', array_nomes, index = None, placeholder="Buscar Item")
+        editInput= st.selectbox('', array_nomes, index = None, placeholder="Editar Item")
 
-st.markdown("##")
+        # FORMS DE EDITAR UM ITEM #
+        if editInput != None:
+            url = f'https://mockapi.up.railway.app/get_item?nome_item={editInput}'
+            response = requests.get(url)
+            item = response.json()
 
-url = 'https://mockapi.up.railway.app/get_itens'
-
-# TABELA DOS ITENS #
-dataTable_container = st.container()
-with dataTable_container:
-   
-    requestData = requests.get(url).json()
-
-    df = pd.DataFrame.from_dict(requestData)
-    df.columns = ['Item', 'Total', 'Em estoque', 'Emprestáveis', 'Emprestados', 'Quebrados', 'Descrição']
-
-    stripInput = searchInput.strip()
-    if stripInput != "":
-        df = df[df["Item"].isin([searchInput])]
-    
-    st.dataframe(df,hide_index=True,) 
-
+            
         
+            with st.form("Editar", True):
+                nome = st.text_input('Nome', value= item["item"])
+                total = st.number_input('Total', value = item["qnt_total"], step=1)
+                estoque = st.number_input('Estoque', value = item["qnt_estoque"], max_value= total, step=1)
+                emprestimo = st.number_input('Emprestáveis', value = item["qnt_emprestimo"],max_value= total, step=1)
+                emprestados = st.number_input('Emprestados', value = item["qnt_emprestados"], max_value= emprestimo,step=1)
+                quebrados = st.number_input('Quebrados',value = item["qnt_quebrados"], max_value= total, step=1)
+                descricao = st.text_input('Descrição', value= item["descricao"])
+
+                submitted = st.form_submit_button("Enviar")
+                if submitted:
+                    data = {
+                        "item": {
+                            "item": item["item"],
+                            "qnt_total": item["qnt_total"],
+                            "qnt_estoque": item["qnt_estoque"],
+                            "qnt_emprestimo": item["qnt_emprestimo"],
+                            "qnt_emprestados": item["qnt_emprestados"],
+                            "qnt_quebrados": item["qnt_quebrados"],
+                            "descricao": item["descricao"]
+                        },
+                        "item2": {
+                            "item": item["item"],
+                            "qnt_total": total,
+                            "qnt_estoque": estoque,
+                            "qnt_emprestimo": emprestimo,
+                            "qnt_emprestados": emprestados,
+                            "qnt_quebrados": quebrados,
+                            "descricao": descricao
+                        }
+                    }
+
+                    url = 'https://mockapi.up.railway.app/edit_item'
+                    response = requests.put(url, json=data)
+                    
+
+
+        # FORMS DE ADICIONAR UM ITEM #
+        with st.expander("Adicionar Item"):
+            with st.form("Adicionar", True):
+                url = 'https://mockapi.up.railway.app/post_item'
+                nome = st.text_input('Nome')
+                total = st.number_input('Total', step=1)
+                estoque = st.number_input('Estoque', step=1)
+                emprestimo = st.number_input('Emprestáveis', step=1)
+                emprestados = st.number_input('Emprestados',  step=1)
+                quebrados = st.number_input('Quebrados', step=1)
+                descricao = st.text_input('Descrição')
+
+            # Every form must have a submit button.
+                submitted = st.form_submit_button("Enviar")
+                if submitted:
+                        data = {
+                            "item" : nome,
+                            "qnt_total": total,
+                            "qnt_estoque": estoque,
+                            "qnt_emprestimo": emprestimo,
+                            "qnt_emprestados": emprestados,
+                            "qnt_quebrados": quebrados,
+                            "descricao": descricao
+                        }
+                        response = requests.post(url, json=data)
+
+            
+
+    st.markdown("##")
+
+    url = 'https://mockapi.up.railway.app/get_itens'
+
+    # TABELA DOS ITENS #
+    dataTable_container = st.container()
+    with dataTable_container:
+    
+        requestData = requests.get(url).json()
+        
+
+        df = pd.DataFrame.from_dict(requestData)
+        df.columns = ['Item', 'Total', 'Em estoque', 'Emprestáveis', 'Emprestados', 'Quebrados', 'Descrição']
+
+
+        if searchInput != None:
+            df = df[df["Item"].isin([searchInput])]
+        
+        st.dataframe(df,hide_index=True,width=1000) 
+
+inventario()
+
+   
