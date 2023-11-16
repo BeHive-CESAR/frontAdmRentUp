7f3a902437c9c2f72717f8da269d536bdc2feb76
CarlosEdu <carlos.mec2005@gmail.com>
2023-11-16 12:34:53 -0300

    Dashboard funcional, falta testar

--- dashboard.py
+++ dashboard.py
@@ -3,8 +3,13 @@ from PIL import Image
 import plotly.express as px
 from api_permissions import get_token
 import plotly.graph_objects as go
+import json
+import requests
 
 def dashboard():
+    token = get_token()
+    headers = {"Authorization": f"Bearer {token}"}
+    
     with open('style.css') as file:
         st.markdown(f'<style>{file.read()}</style>', unsafe_allow_html=True)
 
@@ -26,17 +31,22 @@ def dashboard():
     with st.container():
         cols_up = st.columns([1, 1, 1])
         
+        response = requests.get("https://rentup.up.railway.app/data/dashboard", headers=headers)
+        data = json.loads(response.text)
+        #st.write(data)
+        
         with cols_up[0]:
-            valores = ['345']
+            
+            valores = [data["total_itens"]]
+            
             df = {
-                'Número de Itens' : valores
-                
+                'Número de Itens' : valores    
             }
             
             st.dataframe(df,hide_index=True, use_container_width=True)  # Same as st.write(df)
             
         with cols_up[1]:
-            valores = ['20']
+            valores = [data["total_danificados"]]
             
             df = {
                 'Itens Danificados' : valores
@@ -46,7 +56,7 @@ def dashboard():
             st.dataframe(df,hide_index=True, use_container_width=True)  # Same as st.write(df)
             
         with cols_up[2]:
-            valores = ['15']
+            valores = [data["total_emprestimos_andamento"]]
             
             df = {
                 'Empréstimos em Andamento' : valores
@@ -73,16 +83,22 @@ def dashboard():
         
     with st.container():
         cols_down = st.columns([5, 0.5, 5])
-
-        with cols_down[0]:
+        #st.write(data)
+        
+        with cols_down[0]:      
             
-            colors = ['lightslategray',] * 5
+            colors = ['lightslategray',] * 3
+            valores = [data["itens_mais_emprestados"][0]["qnt_emprestados"], data["itens_mais_emprestados"][1]["qnt_emprestados"], data["itens_mais_emprestados"][2]["qnt_emprestados"]]
+            nomes = [item["nome"] for item in data["itens_mais_emprestados"]]
+           
 
+            # Criar a figura
             fig = go.Figure(data=[go.Bar(
-                x=['Arduíno', 'Arduíno Nano', 'Protoboard', 'Jumper'],
-                y=[20, 14, 23, 25],
-                marker_color=colors # marker color can be a single color value or an iterable
+                x=nomes,
+                y=valores,
+                marker_color='blue'  # Cor do marcador pode ser um valor único ou um iterável
             )])
+            
             fig.update_layout(title_text='Itens Mais Emprestados')
             st.plotly_chart(fig, use_container_width=True)
 
@@ -90,12 +106,15 @@ def dashboard():
         with cols_down[2]:
             #st.markdown('<p class="mediumFont">Mais Emprestados</p>', unsafe_allow_html=True)
             
-            colors = ['lightslategray',] * 5
-
+            colors = ['lightslategray',] * 3
+            
+            valores = [data["itens_mais_danificados"][0]["danificados"], data["itens_mais_danificados"][1]["danificados"], data["itens_mais_danificados"][2]["danificados"]]
+            nomes = [item["nome"] for item in data["itens_mais_danificados"]]
+            
             fig = go.Figure(
                 data=[go.Bar(
-                    x=['Arduíno', 'Arduíno Nano', 'Protoboard', 'Jumper'],
-                    y=[20, 14, 23, 25],
+                    x=nomes,
+                    y=valores,
                     marker_color=colors # marker color can be a single color value or an iterable
             )])
             
